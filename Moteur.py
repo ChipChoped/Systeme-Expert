77034edc9f107353c0ee8bccea994f8fb7bb6d04
Grégoire Bellon <gregoire.bellon@outlook.fr>
2022-11-04 15:52:54 +0100

    debut moteur

---
+++ Moteur.py
@@ -0,0 +1,13 @@
+class Moteur(object):
+
+    #renvoie une base de fait déduite des rêgles
+    #sature les rêgles pour l'instant
+    def chainageAvant(base_de_faits : list, base_de_regles : list)->list:
+        res = True
+        while base_de_regles != [] | res:
+            pass
+
+    # cherche une rêgle afin d'étendre la base de faits
+    # renvoie un tuple (index_regle, [faits_deduits]) ou None
+    def trouverCorrespondanceRegle(base_de_faits : list, base_de_regles : list) -> tuple | None:
+        pass
