e5e7ab49035e6cff6a8b73129f22a25ad5adf36a
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-12-04 00:10:29 +0100

    Update back_chaining_test.py

--- tests/back_chaining_test.py
+++ tests/back_chaining_test.py
@@ -2,18 +2,24 @@ import logging
 
 from Moteur import Moteur
 from Context import Context
-from Datatypes import Element
+from Datatypes import Element, ConcreteRule
 from parser.CustomLexer import CustomLexer
 from parser.CustomParser import CustomParser
 
 
 class TestBackChaining:
+    moteur = Moteur(Context())
+    custom = CustomParser(moteur)
+    lexer = CustomLexer().lexer
+
     def test_success(self):
-        moteur = Moteur(Context())
-        custom = CustomParser(moteur)
-        lexer = CustomLexer().lexer
-        custom.parser.parse('load("ressources/ex2")')
-        logging.info("État du context : \n" + str(moteur.context))
-        hypotheses = moteur.chainageArriere(['BondChampagne', 'ChateauEarl', 'HonestHenryAppleWine', 'ToeLakesRose', 'DosEquis', 'Coors', 'Glops', 'CarrotJuice', 'Water'])
+        self.custom.parser.parse('load("ressources/ex2")')
+        logging.info("État du context : \n" + str(self.moteur.context))
+        hypotheses = self.moteur.chainageArriere(
+            ['BondChampagne', 'ChateauEarl', 'HonestHenryAppleWine', 'ToeLakesRose', 'DosEquis',
+             'Coors', 'Glops', 'CarrotJuice', 'Water'])
         right_hypotheses = [Element(hypothesis, True) for hypothesis in ['HonestHenryAppleWine', 'DosEquis', 'Coors', 'Water']]
         assert hypotheses == right_hypotheses
+
+    def test_fail(self):
+        pass
