e431c83fe3e1d38e345a224e6f7d78ddd45d14a8
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-12-15 21:59:07 +0100

    Affichage du résultats du backward chaining

--- parser/CustomParser.py
+++ parser/CustomParser.py
@@ -1,3 +1,4 @@
+from Context import Context
 from parser.CustomLexer import CustomLexer
 from CoherenceExceptions import RuleCoherenceException
 from Datatypes import Boolean, Metarule, ConcreteRule, Number, EnumElem, Constraint, OperatorTypes, Hypothesis
@@ -7,11 +8,10 @@ import ply.yacc as yacc
 import logging
 
 
-
 class CustomParser(object):
-    
+
     def c_load(self, file_path, *args):
-        file : TextIoWrapper = open(file_path, "r")
+        file: TextIoWrapper = open(file_path, "r")
         for line in file:
             self.parser.parse(line)
 
@@ -19,102 +19,127 @@ class CustomParser(object):
         print(self.moteur.context)
 
     def c_forward(self, hypothese_used, *args):
-        print("chainage avant, avec pour but : "+str(args))
+        print("chainage avant, avec pour but : " + str(args))
 
         hypothese = self.moteur.context.hypothesis.get(hypothese_used)
 
         if hypothese is None:
             raise MissingArguments("L'Hypothèse entrée en paramètre n'existe pas")
-    
+
         contexte_ajoute = self.moteur.chainageAvant(hypothese)
         print(f'Context ajouté : {contexte_ajoute}')
-        
 
-    def c_backward(self, *args):
+    def c_backward(self, hypothese_used, *args):
         print("chainage arrière, avec pour hypothèses : " + str(args))
-        hypotheses_vraies = self.moteur.chainageArriere(args)
-        print(f'Right hypotheses : {hypotheses_vraies}')
+
+        hypotheses = self.moteur.context.hypothesis.get(hypothese_used)
+
+        if hypotheses is None:
+            raise MissingArguments("L'Hypothèse entrée en paramètre n'existe pas")
+
+        contexts = self.moteur.chainageArriere(hypotheses)
+        proven_hypotheses = []
+        disproven_hypotheses = []
+
+        for context in contexts:
+            if context[2]:
+                proven_hypotheses.append(context)
+            else:
+                disproven_hypotheses.append(context)
+
+        print("Hypothèses vraies :\n")
+
+        for hypothesis in proven_hypotheses:
+            print(f'Hypothèse recherchée : {hypothesis[0].elem}')
+            print(f'Faits rencontrés : {dict(reversed(list(hypothesis[1].facts.items())))}')
+            print(f'Règles utilisées : {dict(reversed(list(hypothesis[1].rules.items())))}\n')
+
+        print("Hypothèses fausse :\n")
+
+        for hypothesis in proven_hypotheses:
+            print(f'Hypothèse recherchée : {hypothesis[0].elem}')
+            print(f'Faits rencontrés : {dict(reversed(list(hypothesis[1].facts.items())))}')
+            print(f'Règles utilisées : {dict(reversed(list(hypothesis[1].rules.items())))}\n')
 
     def p_statement(self, p):
         '''statement : 
                     | fonction
-                    | metaregle'''         
+                    | metaregle'''
 
     def p_statement_assignation(self, p):
         '''statement : assignation'''
-        self.moteur.inputFact(p[1])     
+        self.moteur.inputFact(p[1])
         return p
 
-    def p_statement_rule(self,p):
+    def p_statement_rule(self, p):
         ''' statement : regle '''
         self.moteur.inputRule(p[1])
         return p
-    
+
     def p_statement_hypothese(self, p):
         '''statement : hypothese'''
         self.moteur.inputHypothesis(p[1])
         return p
 
-
     def p_fonction_arg(self, p):
         '''fonction : MOT OPEN_PAR argument CLOSE_PAR'''
         logging.debug(f'fonction détectée !, {p[1]}')
-        fun = getattr(self, "c_"+p[1])
+        fun = getattr(self, "c_" + p[1])
         fun(*p[3])
-    
+
     def p_fonction_no_arg(self, p):
         '''fonction : MOT OPEN_PAR CLOSE_PAR'''
-        fun = getattr(self, "c_"+p[1])
+        fun = getattr(self, "c_" + p[1])
         fun()
 
         logging.debug(f'fonction détectée !')
 
-    def p_assignation_value(self,p):
+    def p_assignation_value(self, p):
         '''assignation : MOT EQUALS value'''
-        logging.debug('assignation with value')   
+        logging.debug('assignation with value')
         assignation = p[3]
         assignation.name = p[1]
         p[0] = assignation
         return p
- 
+
     def p_assignation_boolean(self, p):
         '''assignation : boolean'''
-        logging.debug('assignation with boolean')     
+        logging.debug('assignation with boolean')
         p[0] = p[1]
         return p
 
-    def p_enum_list_seul(self,p):
+    def p_enum_list_seul(self, p):
         '''enum_list : boolean'''
-        p[0] = EnumElem("",{p[1].name : p[1]})
+        p[0] = EnumElem("", {p[1].name: p[1]})
         return p
 
-    def p_enum_list_many(self,p):
+    def p_enum_list_many(self, p):
         '''enum_list : boolean COMMA enum_list'''
         p[0] = p[3]
         if not p[0].override(p[1]):
             raise FactCoherenceException("This enumeration is not coherent")
         return p
 
-    def p_enum_with_list(self,p):
+    def p_enum_with_list(self, p):
         '''enum : OPEN_BRACK enum_list CLOSE_BRACK'''
         p[0] = p[2]
         return p
 
     def p_enum_seul(self, p):
         '''enum : boolean'''
-        p[0] = EnumElem(p[1].name, {p[1].name : p[1]})
+        p[0] = EnumElem(p[1].name, {p[1].name: p[1]})
         return p
 
     def p_value_enum(self, p):
         '''value : enum'''
-        logging.debug('enum')  
-        p[0] = p[1]      
+        logging.debug('enum')
+        p[0] = p[1]
         return p
 
     def p_value_number(self, p):
         '''value : NUMBER'''
         logging.debug('number')
-        p[0] = Number("",p[1])    
+        p[0] = Number("", p[1])
         return p
 
     def p_argument_seul(self, p):
@@ -127,7 +152,7 @@ class CustomParser(object):
         '''comparateur : EQUALITY'''
         p[0] = OperatorTypes.EQUALS
         return p
-   
+
     def p_comparateur_greater(self, p):
         '''comparateur : GREATER'''
         p[0] = OperatorTypes.GREATER
@@ -142,7 +167,7 @@ class CustomParser(object):
         '''comparateur : LESS'''
         p[0] = OperatorTypes.LESS
         return p
-        
+
     def p_comparateur_lessOrEqual(self, p):
         '''comparateur : LESS_EQUAL'''
         p[0] = OperatorTypes.LESS_OR_EQUAL
@@ -160,18 +185,18 @@ class CustomParser(object):
         p[0] = [p[1]]
         logging.debug("consequence detected")
         return p
-    
-    def p_consequence_suite(self,p):
+
+    def p_consequence_suite(self, p):
         '''consequence : assignation ET consequence'''
         p[0] = p[1] + p[3]
         return p
 
-    def p_contrainte_bool(self,p):
+    def p_contrainte_bool(self, p):
         '''contrainte : boolean'''
         p[0] = Constraint(p[1], OperatorTypes.EQUALS)
-        return p 
-    
-    def p_contrainte_value(self,p):
+        return p
+
+    def p_contrainte_value(self, p):
         '''contrainte : MOT comparateur value'''
         logging.debug("contrainte detected")
         value = p[3]
@@ -181,7 +206,7 @@ class CustomParser(object):
 
     def p_arguments_ordonnes(self, p):
         '''ordonnes : MOT GREATER ordonnes'''
-        p[0] = [p[1]]+p[3]
+        p[0] = [p[1]] + p[3]
         return p
 
     def p_argument_ordonne_seul(self, p):
@@ -191,13 +216,13 @@ class CustomParser(object):
 
     def p_ensemble_arguments(self, p):
         '''argument : MOT COMMA argument'''
-        p[0] = [p[1]]+p[3]
+        p[0] = [p[1]] + p[3]
         return p
 
     # def p_fait(self, p):
     #     '''fait : element'''
     #     logging.debug(f'fait détecté : [{p[1]}]')
-        
+
     #     self.moteur.inputFact(p[1])
     #     return p 
 
@@ -206,12 +231,12 @@ class CustomParser(object):
         p[0] = Hypothesis(p[1], p[3])
         return p[0]
 
-    def p_hypothese_contenu_alone(self,p):
+    def p_hypothese_contenu_alone(self, p):
         '''hypothese_contenu : premisse'''
         logging.debug(f'Hypothèse détectée')
         p[0] = [ConcreteRule(p[1], [], "")]
-        return p 
-    
+        return p
+
     def p_hypothese_contenu_mult(self, p):
         '''hypothese_contenu : premisse COMMA hypothese_contenu'''
         p[0] = [ConcreteRule(p[1], [], "")] + p[3]
@@ -230,11 +255,10 @@ class CustomParser(object):
         #     return None
         # return p 
 
-
     def p_metaregle_ordonne(self, p):
         '''metaregle : MOT DEUX_POINTS OPEN_BRACK ordonnes CLOSE_BRACK'''
         logging.debug(f'Metaregle détectée : {p[1]}, {p[4]}')
-        meta : Metarule
+        meta: Metarule
         self.moteur.createMetarule(p[1], p[4], True)
         return p
 
@@ -243,13 +267,13 @@ class CustomParser(object):
 
         p[0] = [p[1]] + p[3]
         logging.debug('premisse mult détectée ')
-        return p 
+        return p
 
     def p_premisse_seul(self, p):
         'premisse : contrainte'
         p[0] = [p[1]]
         logging.debug('premisse seule détecté ')
-        return p 
+        return p
 
     def p_boolean_negative(self, p):
         '''boolean : NON MOT'''
@@ -260,7 +284,7 @@ class CustomParser(object):
 
     def p_boolean(self, p):
         'boolean : MOT'
-        
+
         p[0] = Boolean(p[1], True)
         # logging.debug(f'element positif [{p[1]}] detecte')
         return p
@@ -269,8 +293,7 @@ class CustomParser(object):
         print("Syntax error in input!")
         print(p)
 
-    def __init__(self, moteur : Moteur):
+    def __init__(self, moteur: Moteur):
         self.tokens = CustomLexer.tokens
         self.moteur = moteur
         self.parser = yacc.yacc(module=self)
-
